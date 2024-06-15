775294221119ae6129ec0fb71b94b2cfe3cc9e22
Carlos Daniel <carlos.daniel.moura99@gmail.com>
2024-06-15 15:20:58 -0300

    feat(controller): adicionado controladora de usuário
    Adicionado controladora usuário.

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from 'express';
+import type { IUserCreate, IUserService } from '../interface/user.interface';
+import { UserCreateSchema } from '../schema/user.schema';
+
+import HttpResponse from '../utils/HttpResponse';
+import { BadRequestError } from '../utils/helpers/api-errors';
+
+class UserController {
+	private readonly userService: IUserService;
+
+	constructor(userService: IUserService) {
+		this.userService = userService;
+	}
+
+	async create(req: Request, res: Response): Promise<void> {
+		const data: IUserCreate = req.body;
+		const parseResult = UserCreateSchema.safeParse(data);
+
+		if (!parseResult.success) {
+			const errorMessage = parseResult.error.errors
+				.map((error) => `${error.path.join('.')}: ${error.message}`)
+				.join(',');
+
+			throw new BadRequestError(errorMessage);
+		}
+
+		const { email } = parseResult.data;
+
+		const userAlreadyExists = await this.userService.findByEmail(email);
+
+		if (userAlreadyExists) {
+			throw new BadRequestError('E-mail já cadastrado.');
+		}
+
+		const user = await this.userService.create(parseResult.data);
+		res.status(201).send(HttpResponse(201, 'Usuário criado com sucesso.', user));
+	}
+}
+
+export { UserController };

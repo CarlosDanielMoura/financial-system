5a86e5280149d85b237b341b351851badbec2e54
Carlos Daniel <carlos.daniel.moura99@gmail.com>
2024-06-28 11:49:02 -0300

    feat(service): Adicionado na service de user uma nova função.
    Adicionado uma função que busca todos os usuários cadastrados no banco (findAll)

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -6,7 +6,7 @@ class UserServicePrisma implements IUserService {
 	async create(data: IUserCreate): Promise<IUserWithoutPassword> {
 		const { password } = data;
 		const hashPassword = await PasswordHash(password);
-		const userCreated = await prisma.user.create({
+		const userCreated = (await prisma.user.create({
 			data: {
 				...data,
 				password: hashPassword,
@@ -20,12 +20,32 @@ class UserServicePrisma implements IUserService {
 				createdAt: true,
 				updatedAt: true,
 			},
-		});
+		})) as IUserWithoutPassword;
 		return userCreated;
 	}
 
-	findByEmail(email: string): Promise<IUser | null> {
-		const userAlreadyExists = prisma.user.findFirst({
+	async findAll(): Promise<IUserWithoutPassword[] | null> {
+		const usersExists = await prisma.user.findMany({
+			select: {
+				id: true,
+				email: true,
+				name: true,
+				role: true,
+				password: false,
+				createdAt: true,
+				updatedAt: true,
+			},
+		});
+
+		if (usersExists.length === 0) {
+			return null;
+		}
+
+		return usersExists || null;
+	}
+
+	async findByEmail(email: string): Promise<IUser | null> {
+		const userAlreadyExists = await prisma.user.findFirst({
 			where: {
 				email,
 			},
